101a2ad8578a01d28d42954c80b218308546d9e5
Paweł Mikołajczuk <mikolajczuk.private@gmail.com>
2012-03-13 20:29:47 +0100

    v 0.0.3

--- flexpi.js
+++ flexpi.js
@@ -1,9 +1,13 @@
 flex = {
     api_url : 'http://api.flexpi.com:3001/',
     social : { facebook : {} },
+    payment : { cart : {}, transactions: {}, cartData : [], paypal : {} },
     settings : {
         social : {
             facebook : {}
+        },
+        payment : {
+            paypal : {}
         }
     }
 };
@@ -47,19 +51,19 @@ flex.loader = function (c, d) {
 };
 
 flex.connect = function(data, callback) {
-	flex.appData = data;
-	var scripts = [];
-	scripts[0] = flex.api_url+'socket.io/socket.io.js';
+    flex.appData = data;
+    var scripts = [];
+    scripts[0] = flex.api_url+'socket.io/socket.io.js';
 
-	flex.loader(scripts, function () {
-	    flex.socket = io.connect(flex.api_url);
-	    flex.socket.on('connect', function (res) {
+    flex.loader(scripts, function () {
+        flex.socket = io.connect(flex.api_url);
+        flex.socket.on('connect', function (res) {
             flex.readSettings();
-		    if (typeof callback != 'undefined') {
+            if (typeof callback != 'undefined') {
                 callback(res);
             }
-		});
-	});
+        });
+    });
 }
 
 flex.readSettings = function() {
@@ -74,16 +78,17 @@ flex.readSettings = function() {
 
 /**
  * Add new stats 
+ 
  * @param  string   key      defined variable name
  * @param  string   value    saved value 
  * @param  Function callback function called after succesfully save
  */
 flex.stats = function(key, value, callback) {
-	flex.socket.emit('stats', { 
-		'app_id': flex.appData.app_id,
-		'key' : key,
-		'value' : value
-	}, function (res) {
+    flex.socket.emit('stats', { 
+        'app_id': flex.appData.app_id,
+        'key' : key,
+        'value' : value
+    }, function (res) {
         if (typeof callback != 'undefined') {
             callback(res);
         }
@@ -184,9 +189,9 @@ flex.social.facebook.feed = function(data, callback) {
  * Calling flex.social.facebook.login results attempting 
  * to open a popup window. As such, this method should only be 
  * called after a user click event, otherwise the popup window will 
- * be blocked by most browsers.
+ * be blocked by most of browsers.
  * 
- * @param  Function callback function with authentication result
+ * @param  Function callback function with result of authentication 
  * @param  object   scope    comas seperated list of user permissions
  */
 flex.social.facebook.login = function(callback, scope){
@@ -204,7 +209,7 @@ flex.social.facebook.login = function(callback, scope){
 };
 
 /**
- * Log the user out of your site and Facebook
+ * Logout user of your site and Facebook
  * 
  * @param  Function callback function with login status
  */
@@ -219,12 +224,12 @@ flex.social.facebook.logout = function(callback){
 };
 
 /**
- * Multi Friend Selector Dialog allowing the sending user to select multiple recipient users.
- * Requests are only available for Desktop Canvas apps and not websites. 
- * Accepting a request will direct the user to the Canvas Page URL of the app that sent the Request.
+ * Multi Friend Selector Dialog allows sending user to select multiple recipient users.
+ * Requests are only available for Desktop Canvas apps, not websites. 
+ * Accepting a request will direct user to the Canvas Page URL of the app that sent the Request.
  * 
  * @param  Object   data     Parameters for Apprequests Dialog
- * @param  Function callback Callback function with Request Object ID and, an array of the recipient user IDs for the request that was created.
+ * @param  Function callback Callback function with Requests Object ID and, an array of the recipient user IDs for the request that was created.
  */
 flex.social.facebook.requests = function(data, callback){
     if (!flex.social.facebook.isFacebookFrame()) {
@@ -273,7 +278,7 @@ flex.social.facebook.getFb = function(){
 }
 
 /**
- * flex.social.getLoginStatus allows you to determine if a user is logged in to Facebook and has authenticated your app.
+ * flex.social.facebook.getLoginStatus allows you to determine if a user is logged in to Facebook and has authenticated your app.
  * 
  * @param  Function callback function with login status as response
  */
@@ -288,7 +293,7 @@ flex.social.facebook.getLoginStatus = function(callback) {
 };
 
 /**
- * Get loged user data. if user is not logged - it will show login form, and return user data afters succesful login
+ * Get loged user data. if user is not logged - it will show login form, and return user data after succesful login
  * 
  * @param  Function callback Function wit user data as parameter
  */
@@ -310,4 +315,260 @@ flex.social.facebook.getUser = function(callback) {
             }
         });
     });
+}
+
+/* --------------------------- flex.payment--------------------------- */
+
+/**
+ * Init flex.payment - load store.js for localstorage support.
+ * 
+ * @param  Function callback   Function with cart object as response after properly store.js loading.
+ * @param  string   [cartId]   [Optional] Invidual id for cart identification - with this id You can later get transaction status.
+ */
+flex.payment.init = function(callback, cartId) {
+    var scripts = [],
+        date = new Date();
+    scripts[0] = '//flexpi.com/api/store.js';
+
+    flex.loader(scripts, function () {
+        if (typeof cartId == 'undefined') {
+            cartId = date.getTime()+'_'+Math.floor(Math.random()*101);
+        }
+
+        if (typeof store.get('cart') != 'undefined') {
+            flex.payment.cartData = store.get('cart');
+        } else {
+            flex.payment.transactions.cartId = cartId;
+            store.set('transactions', flex.payment.transactions);
+        }
+
+        store.set('cart', flex.payment.cartData);
+        if (typeof callback != 'undefined') {
+            callback(flex.payment.cartData);
+        }
+    });
+}
+
+/**
+ * Get object with cart items.
+ * 
+ * @return object Cart Object
+ */
+flex.payment.cart.get = function() {
+    return flex.payment.cartData;
+}
+
+/**
+ * Get cart Id - with this id You can get transaction status.
+ * 
+ * @return string
+ */
+flex.payment.cart.getId =  function() {
+    if (typeof store != 'undefined') {
+        return store.get('transactions').cartId;
+    } else {
+        return { error: { code: 401, message: "First run 'flex.payment.init'." } }
+    }
+}
+
+/**
+ * Add item to cart
+ * 
+ * @param  string       item_name   Name for product
+ * @param  float        amount      Amount for single item
+ * @param  integer      quantity    Single item quantity
+ * @param  Function     [callback]  [Optional] Function with cart elements as parameter
+ 
+ * @return object       Just added item.
+ */
+flex.payment.cart.add = function(item_name, amount, quantity, callback) {
+    var number = flex.payment.cartData.length;
+    flex.payment.cartData[number] = {
+        'item_name' : item_name,
+        'amount' : amount,
+        'quantity' : quantity
+    };
+    store.set('cart', flex.payment.cartData);
+    if (typeof callback != 'undefined') {
+        callback(flex.payment.cartData);
+    }
+
+    return flex.payment.cartData[number];
+}
+
+/**
+ * Remove item from cart.
+ * 
+ * @param  integer  index    Index from cart array
+ * @param  Function [callback] [Optional] Function with new cart object as parameter
+ */
+flex.payment.cart.remove = function(index, callback) {
+    var temp = [];
+    if (typeof flex.payment.cartData[index] != 'undefined') {
+        delete flex.payment.cartData[index];        
+    }
+
+    for (i=0; i <= flex.payment.cartData.length; i++) {
+        if (typeof flex.payment.cartData[i] != 'undefined') {
+            temp.push(flex.payment.cartData[i]);
+        }
+    }
+
+    flex.payment.cartData = temp;
+    
+    store.set('cart', flex.payment.cartData);
+    if (typeof callback != 'undefined') {
+        callback(flex.payment.cartData);
+    }
+}
+
+/**
+ * Get transaction status.
+ * 
+ * @param  string   transactionId   Saved before cart id - You can get it with flex.payment.cart.getId
+ * @param  Function [callback]      [Optional] Function with transaction status as response
+ */
+flex.payment.transactions.get = function(transactionId, callback) {
+    flex.socket.emit('getPaymentTransaction', { 
+        'app_id': flex.appData.app_id,
+        'transactionId' : transactionId
+    }, function (res) {
+        if (typeof callback != 'undefined') {
+            callback(res);
+        }
+    });
+}
+
+/**
+ * Clear all transactions and cart data from user localstorage.
+ * 
+ * @return bollean/object Returns true if there is no problem and error object if flex.payment.init was not run before
+ */
+flex.payment.transactions.clear = function() {
+    if (typeof store != 'undefined') {
+         store.remove('transactions');
+         store.remove('cart');
+         return true;
+    } else {
+        return { error: { code: 401, message: "First run 'flex.payment.init'." } }
+    }
+}
+
+/**
+ *  Append to container PayPal transaction form.
+ *  But before, You must run flex.payment.init and add elements to cart with flex.payment.cart.add
+ * 
+ * @param  string elementId   Div id - apended form will be here
+ * @param  string buttonValue Text for form submit value
+ */
+flex.payment.paypal.createFormView = function(elementId, buttonValue) {
+    var container   = document.getElementById(elementId);
+
+    if (flex.payment.paypal.init() != true ) {
+        return flex.payment.paypal.init();
+    }
+
+    if (typeof container == 'undefined') {
+        return { error: { code: 404, message: "Element "+elementId+" dont exist." } }
+    }
+
+    if (flex.payment.cartData.length == 0) {
+        return { error: { code: 401, message: "Cart doesn't have any items" } }
+    }
+
+    if (typeof buttonValue == 'undefined') {
+        buttonValue = 'Pay with Paypal';
+    }
+
+    var form        = document.createElement("form");
+        form.setAttribute('action', 'https://www.paypal.com/cgi-bin/webscr');
+        form.setAttribute('method', 'post');
+        form.setAttribute('id', 'flex-payment-paypal');
+
+    var cart        = document.createElement("input"); 
+        cart.setAttribute('hidden', true); 
+        cart.setAttribute('name', 'cmd');
+        cart.setAttribute('value', '_cart');
+
+    var custom      = document.createElement("input");
+        custom.setAttribute('hidden', true);
+        custom.setAttribute('name', 'custom');
+        custom.setAttribute('value', store.get('transactions').cartId+'___'+flex.appData.app_id);
+
+    var upload      = document.createElement("input");
+        upload.setAttribute('hidden', true);
+        upload.setAttribute('name', 'upload');
+        upload.setAttribute('value', 1);
+
+    var currency      = document.createElement("input");
+        currency.setAttribute('hidden', true);
+        currency.setAttribute('name', 'currency_code');
+        currency.setAttribute('value', flex.settings.payment.paypal.currency);
+
+    var notify_url      = document.createElement("input");
+        notify_url.setAttribute('hidden', true);
+        notify_url.setAttribute('name', 'notify_url');
+        notify_url.setAttribute('value', 'http://flexpi.com/api/payment/paypal/ipn');
+
+    var business      = document.createElement("input");
+        business.setAttribute('hidden', true);
+        business.setAttribute('name', 'business');
+        business.setAttribute('value', flex.settings.payment.paypal.email);
+        
+
+    form.appendChild(cart);
+    form.appendChild(custom);
+    form.appendChild(upload);
+    form.appendChild(currency);
+    form.appendChild(business);
+
+    var item_name,
+        amount,
+        quantity;
+
+    for (i=0; i < flex.payment.cartData.length; i++) {
+        item_name      = document.createElement("input");
+        item_name.setAttribute('hidden', true);
+        item_name.setAttribute('name', 'item_name_'+(i+1));
+        item_name.setAttribute('value', flex.payment.cartData[i].item_name);
+
+        amount      = document.createElement("input");
+        amount.setAttribute('hidden', true);
+        amount.setAttribute('name', 'amount_'+(i+1));
+        amount.setAttribute('value', flex.payment.cartData[i].amount.toFixed(2));
+
+        quantity      = document.createElement("input");
+        quantity.setAttribute('hidden', true);
+        quantity.setAttribute('name', 'quantity_'+(i+1));
+        quantity.setAttribute('value', parseInt(flex.payment.cartData[i].quantity, 10));
+
+        form.appendChild(item_name);
+        form.appendChild(amount);
+        form.appendChild(quantity);
+    }
+
+    var submit      = document.createElement("input");
+        submit.setAttribute('type', 'submit');
+        submit.setAttribute('class', 'flex-payment-paypal-send');
+        submit.setAttribute('value', buttonValue);
+
+    form.appendChild(submit);
+
+    container.appendChild(form);
+}
+
+/**
+ * Check PayPal configuration.
+ * 
+ * @return bollean/object Returns true, if there is no problem and error object if PayPal was not configured
+ */
+flex.payment.paypal.init = function(){
+    if (
+        typeof flex.settings.payment.paypal.email == 'undefined' || 
+        typeof flex.settings.payment.paypal.currency == 'undefined'
+    ) {
+        return { error: { code: 401, message: "First configure FlexPayment for PayPal." } }
+    }
+
+    return true;
 }
